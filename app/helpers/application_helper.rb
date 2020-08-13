3f8adc9e60faf2454165232f039356ce4781e3ee
Mark Orr <mark.j.l.orr@googlemail.com>
2020-08-13 15:38:06 +0100

    Index and exhibition pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,21 @@ module ApplicationHelper
     klass.push "offset-xl-#{(12 - xl) / 2} col-xl-#{xl}" if xl > 0
     klass.any? ? klass.join(" ") : "col-12"
   end
+
+  def sample_work
+    file = [
+      "1/a_quiet_street_in_venice_421x600.jpg",
+      "1/cinque_terre_600x809.jpg",
+      "1/cliff_top_houses_cinque_terre_600x414.jpg",
+      "1/provencal_olives_600x450.jpg",
+      "1/st_marks_venice_600x608.jpg",
+      "1/tonys_olives_600x450.jpg",
+      "2/gathering_clouds_at_beadnell_bay_600x433.jpg",
+      "2/pink_socks_stockbridge_600x581.jpg",
+      "2/staffin_bay_skye_600x345.jpg",
+      "3/summer_flowers_600x561.jpg",
+      "3/warm_breeze_600x433.jpg",
+    ].sample
+    "img/gallery" + file
+  end
 end
