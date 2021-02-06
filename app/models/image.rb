20dbdabcf788cd2418fdaaed09db805b94306459
Mark Orr <mark.j.l.orr@googlemail.com>
2021-02-06 17:11:02 +0000

    Add physical dimensions for some pictures

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,11 +1,15 @@
-Image = Struct.new(:g, :s, :file, :size, :type, :name) do
+Image = Struct.new(:g, :s, :file, :px, :cm, :type, :name) do
   def src(tn=false)
-    "img/gallery%d/%s_%s.jpg" % [g, file, tn ? "100x100" : size]
+    "img/gallery%d/%s_%s.jpg" % [g, file, tn ? "100x100" : px]
   end
 
   def alt(full=false)
     if full
-      "<strong>#{name}</strong> (#{I18n.t('type.' + type)})".html_safe
+      details = I18n.t('type.' + type)
+      if cm =~ /\A([1-9]\d*)x([1-9]\d*)\z/
+        details += ", #{$1} x #{$2} cm"
+      end
+      "<strong>#{name}</strong> (#{details})".html_safe
     else
       name
     end
