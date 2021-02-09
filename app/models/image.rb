16b580b0d5598be9da80a2d11dcbeceeb8723cf2
Mark Orr <mark.j.l.orr@googlemail.com>
2021-02-09 12:17:47 +0000

    Replaced model method with partial

--- app/models/image.rb
+++ app/models/image.rb
@@ -2,17 +2,4 @@ Image = Struct.new(:g, :s, :file, :px, :cm, :type, :sold, :name) do
   def src(tn=false)
     "img/gallery%d/%s_%s.jpg" % [g, file, tn ? "100x100" : px]
   end
-
-  def alt(full=false)
-    if full
-      details = I18n.t('type.' + type)
-      if cm =~ /\A([1-9]\d*)x([1-9]\d*)\z/
-        details += ", #{$1} × #{$2} cm"
-      end
-      badge = '<span class="badge badge-pill badge-%s">&nbsp;</badge>' % [sold == 's' ? "danger" : "success"]
-      "<strong>#{name}</strong> (#{details}) #{badge}".html_safe
-    else
-      name
-    end
-  end
 end
