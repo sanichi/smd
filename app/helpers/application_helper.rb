ff7693541dcdd03f77e02374f8bc6b7b4c5914a8
Mark Orr <mark.j.l.orr@googlemail.com>
2021-07-24 13:00:43 +0100

    Tweaks

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -127,6 +127,15 @@ module ApplicationHelper
     1 + (pics.index{ |p| p.file == file } || 0)
   end
 
+  def pagination_links(pager)
+    links = Array.new
+    links.push(link_to t("pagination.frst"), pager.frst_page, remote: pager.remote, id: "pagn_frst") if pager.after_start?
+    links.push(link_to t("pagination.next"), pager.next_page, remote: pager.remote, id: "pagn_next") if pager.before_end?
+    links.push(link_to t("pagination.prev"), pager.prev_page, remote: pager.remote, id: "pagn_prev") if pager.after_start?
+    links.push(link_to t("pagination.last"), pager.last_page, remote: pager.remote, id: "pagn_last") if pager.before_end?
+    raw "#{pager.min_and_max} #{t('pagination.of')} #{pager.count} #{links.size > 0 ? '∙' : ''} #{links.join(t("pagination.sep"))}"
+  end
+
   def center(xs: 0, sm: 0, md: 0, lg: 0, xl: 0, xx: 0)
     klass = []
     klass.push "offset-#{(12 - xs) / 2} col-#{xs}"         if xs > 0
