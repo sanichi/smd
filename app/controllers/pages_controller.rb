ca18f728ada41ad279dc4a04845e9f88202f1468
Mark Orr <mark.j.l.orr@googlemail.com>
2021-07-31 15:34:23 +0100

    Get rid of legacy stuff

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,21 +1,9 @@
 class PagesController < ApplicationController
-  before_action :get_file, only: [:gallery1, :gallery2, :gallery3, :gallery4]
-
   def index
     @sample = Painting.sample
   end
 
   def exhibitions
     @sample = Painting.sample
-  end
-
-  def search
-    @name = params[:name]
-  end
-
-  private
-
-  def get_file
-    @file = params[:f]
   end
 end
