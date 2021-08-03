63baa2ea8935f2fd86c6aaa339af1a818cec1ba1
Mark Orr <mark.j.l.orr@googlemail.com>
2021-08-03 15:34:38 +0100

    More tests

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -10,6 +10,7 @@ class PagesController < ApplicationController
   end
 
   def test
+    @me = %x|whoami|
     @which = %x|which convert|
     @jpg = Rails.root + "public" + "img" + "jpg.jpg"
     @jpg_c = %x|convert /tmp/img.jpg -resize '100x100!' #{@jpg}|
