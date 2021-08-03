0a61ede7b28b1040decd12eb99a4735939fb0cbd
Mark Orr <mark.j.l.orr@googlemail.com>
2021-08-03 15:17:57 +0100

    Test

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -9,6 +9,18 @@ class PagesController < ApplicationController
     @sample = Painting.sample
   end
 
+  def test
+    @jpg = Rails.root + "public" + "img" + "jpg.jpg"
+    @jpg_c = %x|convert /tmp/img.jpg -resize '100x100!' #{@jpg}|
+    @jpg_i = %x|identify #{@jpg}|
+    @png = Rails.root + "public" + "img" + "png.jpg"
+    @png_c = %x|convert /tmp/img.png -resize '100x100!' #{@png}|
+    @png_i = %x|identify #{@png}|
+    @gif = Rails.root + "public" + "img" + "gif.jpg"
+    @gif_c = %x|convert /tmp/img.png -resize '100x100!' #{@gif}|
+    @gif_i = %x|identify #{@gif}|
+  end
+
   private
 
   def markdown(name)
