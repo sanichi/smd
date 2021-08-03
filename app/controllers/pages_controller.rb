8389824951153a3ee05bda534d24f7f5c2e737b6
Mark Orr <mark.j.l.orr@googlemail.com>
2021-08-03 15:52:31 +0100

    More tests

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -13,13 +13,13 @@ class PagesController < ApplicationController
     @me = %x|whoami 2>&1|
     @which = %x|which convert 2>&1|
     @jpg = Rails.root + "public" + "img" + "jpg.jpg"
-    @jpg_c = %x|convert /tmp/img.jpg -resize '100x100!' #{@jpg} 2>&1|
+    @jpg_c = %x|convert /home/sanichi/img.jpg -resize '100x100!' #{@jpg} 2>&1|
     @jpg_i = %x|identify #{@jpg} 2>&1|
     @png = Rails.root + "public" + "img" + "png.jpg"
-    @png_c = %x|convert /tmp/img.png -resize '100x100!' #{@png} 2>&1|
+    @png_c = %x|convert /home/sanichi/img.png -resize '100x100!' #{@png} 2>&1|
     @png_i = %x|identify #{@png} 2>&1|
     @gif = Rails.root + "public" + "img" + "gif.jpg"
-    @gif_c = %x|convert /tmp/img.png -resize '100x100!' #{@gif} 2>&1|
+    @gif_c = %x|convert /home/sanichi/img.png -resize '100x100!' #{@gif} 2>&1|
     @gif_i = %x|identify #{@gif} 2>&1|
   end
 
