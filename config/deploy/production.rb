f0a29c100bda5a8ac45c05a5e710e88e1b741854
Mark Orr <mark.j.l.orr@googlemail.com>
2022-02-27 16:14:47 +0000

    Prepare to relocate to nihon

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
-server "thessaloniki.sanichi.me", user: "sanichi", roles: %w{web app db}
+server "nihon.sanichi.me", user: "sanichi", roles: %w{web app db}
 set :deploy_to, "/var/www/smd"
 set :rails_env, "production"
 
