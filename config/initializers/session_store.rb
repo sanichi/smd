89da22037af0459ae4790dffc06af26fcde5efe7
Mark Orr <mark.j.l.orr@googlemail.com>
2022-03-09 12:08:41 +0000

    Fix for CSRF in dev environment problem

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,4 +1,4 @@
 # Be sure to restart your server when you modify this file.
 Rails.application.config.session_store :cookie_store,
-  key: "__Secure-Smd-Session",
+  key: "#{Rails.env.production? ? '__Secure-' : ''}Smd-Session",
   expire_after: 4.weeks
