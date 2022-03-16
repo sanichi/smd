f41fafddbbddeffb051c9518c2de39aff11063a7
Mark Orr <mark.j.l.orr@googlemail.com>
2022-03-16 09:22:55 +0000

    Get rid of some old stuff

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,42 +16,6 @@
 //= require bootstrap
 //= require clipboard
 
-// click controls
-function select_work(num) {
-  $(".work").hide();
-  $("#work" + num).show();
-  $(".selector").removeClass("selected");
-  $("#selector" + num).addClass("selected");
-  $("#num").text(num);
-}
-
-// keyboard controls
-$(window).keydown(function (e) {
-  switch (e.keyCode) {
-    case 39: // right arrow
-      select_next(1);
-      break;
-    case 37: // left arrow
-      select_next(-1);
-      break;
-  }
-});
-
-// keyboard and button controls
-function select_next(delta) {
-  var id = $('.selector.selected').attr('id');
-  if (id) {
-    var match = id.match(/[1-9]\d*/);
-    if (match) {
-      num = parseInt(match[0]) + delta;
-      var total = $('.selector').length;
-      num = num > total ? 1 : num;
-      num = num < 1 ? total : num;
-      select_work(num);
-    }
-  }
-}
-
 $(function () {
   // Auto-submit on change.
   $('form .auto-submit').change(function () {
