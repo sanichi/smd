3679d53251f6f9fb6dddb91e8392584830731f3f
Mark Orr <mark.j.l.orr@googlemail.com>
2025-01-17 08:42:32 +0000

    Temporary solution to pages spec js problem

--- spec/features/pages_spec.rb
+++ spec/features/pages_spec.rb
@@ -1,62 +1,66 @@
 require 'rails_helper'
 
-# TODO - this doesn't work with js, can't find Galleries button/link
 describe PagesController do
   before(:each) do
     visit index_path
   end
 
-  it "index" do
-    expect(page).to have_title t("pages.index.title")
-  end
-
-  it "gallery 1" do
-    within "#gallery-dropdown" do
-      click_link t("pages.gallery1.title")
+  context "works with js", js: true do
+    it "index" do
+      expect(page).to have_title t("pages.index.title")
     end
-
-    expect(page).to have_title t("pages.gallery1.title")
-  end
-
-  it "gallery 2" do
-    within "#gallery-dropdown" do
-      click_link t("pages.gallery2.title")
+  
+    it "available" do
+      click_link t("pages.available.title")
+  
+      expect(page).to have_title t("pages.available.title")
     end
-
-    expect(page).to have_title t("pages.gallery2.title")
-  end
-
-  it "gallery 3" do
-    within "#gallery-dropdown" do
-      click_link t("pages.gallery3.title")
+  
+    it "sale" do
+      click_link t("pages.sale.title")
+  
+      expect(page).to have_title t("pages.sale.title")
     end
-
-    expect(page).to have_title t("pages.gallery3.title")
-  end
-
-  it "gallery 4" do
-    within "#gallery-dropdown" do
-      click_link t("pages.gallery4.title")
+  
+    it "exhibitions" do
+      click_link t("pages.exhibitions.title")
+  
+      expect(page).to have_title t("pages.exhibitions.title")
     end
-
-    expect(page).to have_title t("pages.gallery4.title")
   end
 
-  it "available" do
-    click_link t("pages.available.title")
-
-    expect(page).to have_title t("pages.available.title")
-  end
-
-  it "sale" do
-    click_link t("pages.sale.title")
-
-    expect(page).to have_title t("pages.sale.title")
-  end
-
-  it "exhibitions" do
-    click_link t("pages.exhibitions.title")
-
-    expect(page).to have_title t("pages.exhibitions.title")
+  # FIX: even with addition of click_link(t("galleries")) the gallery title links can't be found when js is on
+  context "doesn't work with js", js: false do
+    it "gallery 1" do
+      within "#gallery-dropdown" do
+        click_link t("pages.gallery1.title")
+      end
+  
+      expect(page).to have_title t("pages.gallery1.title")
+    end
+  
+    it "gallery 2" do
+      within "#gallery-dropdown" do
+        click_link t("pages.gallery2.title")
+      end
+  
+      expect(page).to have_title t("pages.gallery2.title")
+    end
+  
+    it "gallery 3" do
+      within "#gallery-dropdown" do
+        click_link t("pages.gallery3.title")
+      end
+  
+      expect(page).to have_title t("pages.gallery3.title")
+    end
+  
+    it "gallery 4" do
+      within "#gallery-dropdown" do
+        click_link t("pages.gallery4.title")
+      end
+  
+      expect(page).to have_title t("pages.gallery4.title")
+    end
   end
 end
