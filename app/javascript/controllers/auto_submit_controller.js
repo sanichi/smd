4ce25e42e753caba6a80b319a2b1d5164283a86d
Mark Orr <mark.j.l.orr@googlemail.com>
2025-01-29 17:44:20 +0000

    Improved auto-submit

--- app/javascript/controllers/auto_submit_controller.js
+++ app/javascript/controllers/auto_submit_controller.js
@@ -1,9 +1,7 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  connect() {
-    this.element.addEventListener("change", function (e) {
-      this.form.requestSubmit();
-    });
+  go() {
+    this.element.form.requestSubmit();
   }
 }
