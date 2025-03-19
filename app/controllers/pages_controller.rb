36586e882d11be03e0224d8643622e8d6a1470ff
Mark Orr <mark.j.l.orr@googlemail.com>
2025-03-19 18:45:25 +0000

    Fix postgres version

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,7 +8,7 @@ class PagesController < ApplicationController
     vers = dirs.scan(/\d*\.\d*\.\d*/)
     @passenger_version = vers.any? ? vers.last : "not found"
     vers = ActiveRecord::Base.connection.execute('select version();').values[0][0] rescue "oops"
-    @postgres_version = vers.match(/(1[4-6]\.\d+)/)? $1 : "not found"
+    @postgres_version = vers.match(/PostgreSQL (1[4-8]\.\d+)/)? $1 : "not found"
     @host = ENV["HOSTNAME"] || `hostname`.chop.sub(".local", "")
   end
 
