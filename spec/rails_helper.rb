21fc1843fdcc0e06b1bc19701f73eaf708414857
Mark Orr <mark.j.l.orr@googlemail.com>
2025-04-02 15:23:43 +0100

    No longer need selenium fix in rails_helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -68,7 +68,7 @@ Capybara.configure do |config|
 end
 
 # Temporary solution to selenium deprecation messages.
-Selenium::WebDriver.logger.ignore(:clear_local_storage, :clear_session_storage)
+# Selenium::WebDriver.logger.ignore(:clear_local_storage, :clear_session_storage)
 
 def login(user)
   visit sign_in_path
