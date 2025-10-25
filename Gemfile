1832aaa62ecd6bafa59b0551cad863d4b71a331c
Mark Orr <mark.j.l.orr@googlemail.com>
2025-10-25 16:25:33 +0100

    Bundle update

--- Gemfile
+++ Gemfile
@@ -16,6 +16,7 @@ gem 'sprockets-rails', '~> 3.4'
 gem "importmap-rails", "~> 2.1"
 gem "turbo-rails", "~> 2.0"
 gem "stimulus-rails", "~> 1.3"
+gem 'sni', git: 'https://github.com/sanichi/sni'
 
 group :development, :test do
   gem 'rspec-rails', '< 9'
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,11 +4,7 @@ class PagesController < ApplicationController
   end
 
   def env
-    @passenger_version = `env -i /usr/bin/passenger-config --version`.scan(/\d+\.\d+\.\d+/).first if Rails.env.production?
-    psql = ActiveRecord::Base.connection.execute('select version();').values[0][0] rescue "oops"
-    @postgres_version = psql.match(/PostgreSQL (1[4-8]\.\d+)/)? $1 : "not found"
-    @puma_version = Puma::Const::VERSION if Rails.env.development?
-    @host = ENV["HOSTNAME"] || `hostname`.chop.sub(".local", "")
+    @sys_info = Sni::SysInfo.call
   end
 
   def exhibitions
