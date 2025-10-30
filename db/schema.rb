f0d73f0616095c620bf0385097b8350a35e8cca7
Mark Orr <mark.j.l.orr@googlemail.com>
2025-10-30 16:12:03 +0000

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,66 +10,65 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_15_155146) do
+ActiveRecord::Schema[8.1].define(version: 2022_11_15_155146) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension "pg_catalog.plpgsql"
 
   create_table "contacts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "email", limit: 100
     t.string "first_name", limit: 50
     t.string "last_name", limit: 50
-    t.string "email", limit: 100
-    t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "contents", force: :cascade do |t|
-    t.string "name", limit: 20
-    t.text "markdown"
     t.datetime "created_at", null: false
+    t.text "markdown"
+    t.string "name", limit: 20
     t.datetime "updated_at", null: false
   end
 
   create_table "exhibits", force: :cascade do |t|
-    t.string "name", limit: 40
+    t.datetime "created_at", null: false
     t.string "link"
     t.string "location", limit: 40
-    t.boolean "previous", default: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string "name", limit: 40
     t.integer "paintings_count", limit: 2, default: 0
+    t.boolean "previous", default: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "paintings", force: :cascade do |t|
-    t.string "title"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer "width", limit: 2
-    t.integer "height", limit: 2
-    t.string "media", limit: 5
-    t.boolean "sold", default: false
-    t.integer "gallery", limit: 2
-    t.integer "image_width", limit: 2
-    t.integer "image_height", limit: 2
-    t.integer "price", limit: 2
-    t.integer "stars", limit: 2
     t.boolean "archived", default: false
-    t.integer "version", limit: 2, default: 0
-    t.integer "print", limit: 2
+    t.datetime "created_at", null: false
     t.bigint "exhibit_id"
-    t.boolean "sale", default: false
+    t.integer "gallery", limit: 2
+    t.integer "height", limit: 2
+    t.integer "image_height", limit: 2
+    t.integer "image_width", limit: 2
+    t.string "media", limit: 5
     t.text "note"
+    t.integer "price", limit: 2
+    t.integer "print", limit: 2
+    t.boolean "sale", default: false
+    t.boolean "sold", default: false
+    t.integer "stars", limit: 2
+    t.string "title"
+    t.datetime "updated_at", null: false
+    t.integer "version", limit: 2, default: 0
+    t.integer "width", limit: 2
     t.index ["exhibit_id"], name: "index_paintings_on_exhibit_id"
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "password_digest"
     t.boolean "admin", default: false
     t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.integer "last_otp_at"
+    t.string "name"
     t.boolean "otp_required", default: false
     t.string "otp_secret", limit: 32
-    t.integer "last_otp_at"
+    t.string "password_digest"
+    t.datetime "updated_at", null: false
   end
-
 end
